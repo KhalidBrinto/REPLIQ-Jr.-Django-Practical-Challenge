26f69a409611e242451945a0e4777b6b24d9df17
KhalidBrinto <kbsbrinto@gmail.com>
2023-06-04 22:43:47 +0600

    finalize prototype project

--- CorporateAssetTracker/AssetTrackerApp/admin.py
+++ CorporateAssetTracker/AssetTrackerApp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import Asset, Employee, AssetAssignment
 
-# Registering models to populate them from admin panel.
+# Registering models to populate them from admin panel and
+# also performing CRUD operations on models.
 # username: admin, password: djdjdj12345
 admin.site.register(Asset)
 admin.site.register(Employee)
